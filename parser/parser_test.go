ceccd64802d2f7ee96c3212d957ff089174e1cca
jasselin <jason.asselin@cdid.com>
2017-07-27 20:46:35 -0400

    Add String method to statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -73,11 +73,9 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 }
 
 func TestReturnStatements(t *testing.T) {
-	input := `
-return 5;
-return 10;
-return 993322;
-`
+	input := `return 5;
+			  return 10;
+			  return 993322;`
 
 	l := lexer.New(input)
 	p := New(l)
