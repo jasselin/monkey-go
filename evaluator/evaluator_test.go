60462553ccb25951e6a37b5b73685b5589ab32fe
jasselin <jason.asselin@cdid.com>
2017-07-31 21:53:21 -0400

    Implement len builtin function for strings

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -339,3 +339,54 @@ func TestStringConcatenation(t *testing.T) {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
+
+func TestBuiltinFunctions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`len("")`, 0},
+		{`len("four")`, 4},
+		{`len("hello world")`, 11},
+		{`len(1)`, "argument to `len` not supported, got INTEGER"},
+		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+		case nil:
+			testNullObject(t, evaluated)
+		case string:
+			errObj, ok := evaluated.(*object.Error)
+			if !ok {
+				t.Errorf("object is not Error. got=%T (%+v)",
+					evaluated, evaluated)
+				continue
+			}
+			if errObj.Message != expected {
+				t.Errorf("wrong error message. expected=%q, got=%q",
+					expected, errObj.Message)
+			}
+			// case []int:
+			// 	array, ok := evaluated.(*object.Array)
+			// 	if !ok {
+			// 		t.Errorf("obj not Array. got=%T (%+v)", evaluated, evaluated)
+			// 		continue
+			// 	}
+
+			// 	if len(array.Elements) != len(expected) {
+			// 		t.Errorf("wrong num of elements. want=%d, got=%d",
+			// 			len(expected), len(array.Elements))
+			// 		continue
+			// 	}
+
+			// 	for i, expectedElem := range expected {
+			// 		testIntegerObject(t, array.Elements[i], int64(expectedElem))
+			// 	}
+		}
+	}
+}
