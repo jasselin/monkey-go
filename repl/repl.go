87bedb5266205f09ebaeb90ed921fb95cf6600c8
jasselin <jason.asselin@cdid.com>
2017-08-03 09:50:41 -0400

    Add puts builtin function and automatically add a semicolon when missing in the REPL

--- repl/repl.go
+++ repl/repl.go
@@ -24,6 +24,10 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
+		if line[len(line)-1] != ';' {
+			line = line + ";"
+		}
+
 		l := lexer.New(line)
 		p := parser.New(l)
 
